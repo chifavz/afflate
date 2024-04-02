e98ad37d664defee7087e5a86b450e4c43de0663
chike ibekwe <chifavz@gmail.com>
2024-04-02 11:52:44 -0500

    Added image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,21 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 
 // Step 2: Define your component
 const HomePage = () => {
   return (
     
-        <Layout pageTitle="Fav Review">
+        <Layout pageTitle="ISGO STUDIO REVIEW">
       
-      <h2>Welcome to FavzReview Your Ultimate Guide to Tech Gadgets and Accessories!</h2>
-      <p>At FavReview, we're passionate about technology and helping you find the best gadgets and accessories to enhance your digital lifestyle. Whether you're a tech enthusiast, a casual user, or someone looking for the perfect gift, we've got you covered.</p>
+      <h2>Welcome to ISGO STUDIO REVIEW Your Ultimate Guide to Tech Products and Accessories!</h2>
+      <StaticImage
+      alt="gadgates"
+      src="../images/techgadget.png" />
+
+      <p>At Isgo Studio Review, we're passionate about technology and helping you find the best products and accessories to enhance your digital lifestyle. Whether you're a tech enthusiast, a casual user, or someone looking for the perfect gift, we've got you covered.</p>
      
     </Layout>
   )
