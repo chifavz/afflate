73aa48992763ad95c70cfff6c199e30b72db959b
chike ibekwe <chifavz@gmail.com>
2024-04-10 14:57:03 -0500

    added blog files

---
+++ src/pages/blog.js
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="Posts">
+      <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+      </ul>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
+
+export const Head = () => <Seo title="Posts" />
+
+export default BlogPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,27 +2,28 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
 
 
 // Step 2: Define your component
 const HomePage = () => {
   return (
     
-        <Layout pageTitle="ISGO STUDIO REVIEW">
+        <Layout pageTitle="Welcome to Chifavz Studio Your Ultimate Guide to Tech Products and Accessories!">
       
-      <h2>Welcome to ISGO STUDIO REVIEW Your Ultimate Guide to Tech Products and Accessories!</h2>
+      
       <StaticImage
       alt="gadgates"
       src="../images/techgadget.png" />
 
-      <p>At Isgo Studio Review, we're passionate about technology and helping you find the best products and accessories to enhance your digital lifestyle. Whether you're a tech enthusiast, a casual user, or someone looking for the perfect gift, we've got you covered.</p>
+      <h2>At Chifavz Studio, we're passionate about technology and helping you find the best products and accessories to enhance your digital lifestyle. Whether you're a tech enthusiast, a casual user, or someone looking for the perfect gift, we've got you covered.</h2>
      
     </Layout>
   )
 }
 
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Home Page" />
 
 // Step 3: Export your component
 export default HomePage
