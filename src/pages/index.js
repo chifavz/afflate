ee1c7864fe4b2c6a698dc963ba2d3a8e7d3d8afc
chike ibekwe <chifavz@gmail.com>
2024-04-20 18:25:41 -0500

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,15 @@ import Seo from '../components/seo'
 
 const HomePage = ({ data }) => {
   return (
-    <Layout pageTitle="Welcome to Chifavz Studio Your Ultimate Guide to Tech Products and Accessories!">
+    <Layout pageTitle="Chifavz Studio Tech Review">
       <Seo title="Home Page" />
       <StaticImage
         alt="gadgets"
         src="../images/techgadget.png"
       />
-      <h2>At Chifavz Studio, we're passionate about technology and helping you find the best products and accessories to enhance your digital lifestyle. Whether you're a tech enthusiast, a casual user, or someone looking for the perfect gift, we've got you covered.</h2>
+      <h2>Discover the Latest in Tech with Chifavz Studio</h2>
+<p>At Chifavz Studio, we're dedicated to providing insightful reviews and recommendations on the latest technology products. Whether you're a tech enthusiast seeking the newest gadgets, a casual user looking for reliable devices, or someone searching for the perfect tech gift, we've got you covered. Explore our reviews to find the best tech solutions for your digital lifestyle.</p>
+
       <h3>Latest Posts:</h3>
       <ul>
         {data.allMdx.nodes.map(node => (
