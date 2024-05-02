9de374f1cc37d5a8368e152da5b132c973cc26a0
chike ibekwe <chifavz@gmail.com>
2024-05-01 22:00:08 -0500

    added the correct path

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,23 +4,20 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
-    title:'Chifavz Studio'
+    title: 'Chifavz Studio'
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-    
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `post`,
-        path: `${__dirname}/posts`,
-
+        path: `${__dirname}/post`
       }
     },
     'gatsby-plugin-mdx',
-    "gatsby-transformer-sharp",
+    "gatsby-transformer-sharp"
+  ]
+};
 
-  ],
-
-}
